3b429684b96bc4f2f7a7a3fd9a14e7d9a8b51e83
Sadialiou Diallo <seriadiallo1@gmail.com>
2023-08-30 11:30:46 +0000

    return json after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -13,13 +14,18 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        $token = $request->user()->createToken($request->user()->name);
+
+        return response()->json(['token' => $token->plainTextToken]);
+
+
+        // return response()->noContent();
     }
 
     /**
